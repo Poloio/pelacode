bb696d501b589ba9ae7bdf98e2e0f432f870f36c
Poloio <carlos@iesnervion.es>
2021-04-12 21:58:44 +0200

    Ej 2 Actualizaciones

--- SQL/northwind_actualizaciones.sql
+++ SQL/northwind_actualizaciones.sql
@@ -4,9 +4,6 @@ GO
 --2 Vender (hoy) 3 de Pavlova, 10 de Inlagd Sil y 25 de Filo Mix a un cliente.
 --Distribuidor Speedy Express y vendedor Laura Callahan.
 
-USE [Northwind]
-GO
-
 INSERT INTO [dbo].[Orders]
            ([CustomerID]
            ,[EmployeeID]
@@ -22,22 +19,48 @@ INSERT INTO [dbo].[Orders]
            ,[ShipPostalCode]
            ,[ShipCountry])
      VALUES
-           ('ALFKI'
+           (
+		   'ALFKI'
            ,(SELECT EmployeeID FROM Employees WHERE FirstName = 'Laura' AND LastName = 'Callahan')
            ,CURRENT_TIMESTAMP
            ,DATEADD(MONTH, 1, CURRENT_TIMESTAMP)
            ,NULL
            ,1
-           ,30,38
+		   ,38
            ,'EVERGREEN'
-           ,'Obere Str. 57'
-           ,<ShipCity, nvarchar(15),>
-           ,<ShipRegion, nvarchar(15),>
-           ,<ShipPostalCode, nvarchar(10),>
-           ,<ShipCountry, nvarchar(15),>)
+           ,(SELECT Address FROM Customers WHERE CustomerID = 'ALFKI')
+           ,(SELECT City FROM Customers WHERE CustomerID = 'ALFKI')
+           ,(SELECT Region FROM Customers WHERE CustomerID = 'ALFKI')
+           ,(SELECT PostalCode FROM Customers WHERE CustomerID = 'ALFKI')
+           ,(SELECT Country FROM Customers WHERE CustomerID = 'ALFKI')
+		   )
 GO
-select * from Orders
-select * from Customers where CustomerID = 'ALFKI'
 
-select
+DECLARE @OrderID int
+SELECT @OrderID = @@IDENTITY
 
+INSERT INTO [dbo].[Order Details]
+           ([OrderID]
+           ,[ProductID]
+           ,[UnitPrice]
+           ,[Quantity]
+           ,[Discount])
+     VALUES
+           (@OrderID
+           ,(SELECT ProductID FROM Products WHERE ProductName = 'Pavlova')
+           ,(SELECT UnitPrice FROM Products WHERE ProductName = 'Pavlova')
+           ,3
+           ,0)
+		   ,
+           (@OrderID
+           ,(SELECT ProductID FROM Products WHERE ProductName = 'Inlagd Sil')
+           ,(SELECT UnitPrice FROM Products WHERE ProductName = 'Inlagd Sil')
+           ,10
+           ,0)
+		   ,
+           (@OrderID
+           ,(SELECT ProductID FROM Products WHERE ProductName = 'Filo Mix')
+           ,(SELECT UnitPrice FROM Products WHERE ProductName = 'Filo Mix')
+           ,25
+           ,0)
+GO
