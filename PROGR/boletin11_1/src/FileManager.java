680dab36159c3e8a053b9541d9a5f2bb4dc86481
Poloio <carlos.aragon@iesnervion.es>
2021-04-29 19:50:39 +0200

    Terminado hasta el ejercicio 3, repasar cómo conseguir propiedades

--- PROGR/boletin11_1/src/FileManager.java
+++ PROGR/boletin11_1/src/FileManager.java
@@ -2,6 +2,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class FileManager {
@@ -9,7 +12,7 @@ public class FileManager {
     public FileManager() {}
 
     public void findFilesForFolder(final File directory) {
-        for (File fileEntry : directory.listFiles()) {
+        for (File fileEntry : Objects.requireNonNull(directory.listFiles())) {
             if (fileEntry.isDirectory()) {
                 findFilesForFolder(fileEntry);
             } else {
@@ -20,10 +23,10 @@ public class FileManager {
 
     //Lee el archivo caracter a caracter para una representación exacta del texto.
     String getFileContentFR(final File file) {
-        StringBuffer buffer = new StringBuffer();
+        StringBuilder buffer = new StringBuilder();
         try {
             FileReader reader = new FileReader(file);
-            int i = 0;
+            int i;
             while ((i=reader.read()) != -1){
                 buffer.append((char) i);
             }
@@ -37,16 +40,41 @@ public class FileManager {
 
     //Lee el archivo línea a línea y pueden parsearse primitivos
     String getFileContentSC(final File file) {
-        StringBuffer buffer = new StringBuffer();
+        StringBuilder builder = new StringBuilder();
         try {
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
-                buffer.append(scanner.nextLine());
+                builder.append(scanner.nextLine());
             }
             scanner.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        return buffer.toString();
+        return builder.toString();
+    }
+
+    String[] getContentAsList(String path) {
+        List<String> outList = new LinkedList<>();
+        int palabras = 1; //Necesaria para sacar un array de String del método
+        try (FileReader target = new FileReader(path)) {
+            int i;
+            StringBuilder strbuild = new StringBuilder();
+            //Mientras no se encuentre el fin del archivo
+            while ((i = target.read()) != -1) {
+                //SI es un espacio o salto de linea se guarda la palabra;
+                if ((char)i == '\n' || (char)i == ' ') {
+                    outList.add(strbuild.toString());
+                    strbuild.setLength(0);
+                    palabras++;
+                } else {
+                    strbuild.append((char)i);
+                }
+            }
+            //Cuando se llega al fin del archivo se añade la última palabra
+            outList.add(strbuild.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return outList.toArray(new String[palabras]);
     }
 }
--- PROGR/boletin11_1/src/Main.java
+++ PROGR/boletin11_1/src/Main.java
@@ -7,9 +7,14 @@ public class Main {
         File root = new File("./src");
         fm.findFilesForFolder(root);
 
-        File file = new File("./src/jaja.txt");
+        File file = new File("./src/files/jaja.txt");
         System.out.println(fm.getFileContentSC(file));
         System.out.println();
         System.out.println(fm.getFileContentFR(file));
+
+        String[] listaA = fm.getContentAsList("./src/files/A.txt");
+        System.out.println(listaA[3]);
+        String[] listaB = fm.getContentAsList("./src/files/B.txt");
+        System.out.println(listaB[3]);
     }
 }
