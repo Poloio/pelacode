8ad38598bb193c5021cd830bc8542f1cc729a558
Poloio <carlos.aragon@iesnervion.es>
2021-05-04 11:06:06 +0200

    Avanzadoe n boletin 11_0 bbddd

---
+++ SQL/programacion_bbdd/boletin11_0.sql
@@ -0,0 +1,79 @@
+USE LeoMetro
+GO
+/*
+-1-
+Crea una función inline que nos devuelva el número de estaciones 
+que ha recorrido cada tren en un determinado periodo de tiempo. 
+El principio y el fin de ese periodo se pasarán como parámetros
+*/
+
+CREATE OR ALTER FUNCTION estacionesRecorridas(@IDTren int, @Principio smalldatetime, @Fin smalldatetime)
+RETURNS int AS
+BEGIN
+	DECLARE @Estaciones int
+	SELECT @Estaciones = ISNULL(COUNT(estacion), 0) FROM LM_Recorridos
+	WHERE
+		Momento BETWEEN @Principio AND @Fin
+		AND Tren = @IDTren
+	GROUP BY Tren
+
+	RETURN @Estaciones
+END
+GO
+
+SELECT dbo.estacionesRecorridas(110,'24-02-2017','28-02-2017') as estaciones
+GO
+
+/*
+-2-
+Crea una función inline que nos devuelva el número de veces que cada usuario 
+ha entrado en el metro en un periodo de tiempo. El principio y el fin de ese periodo 
+se pasarán como parámetros
+*/
+
+CREATE OR ALTER FUNCTION subidasUsuarios(@Principio smalldatetime, @Fin smalldatetime)
+RETURNS TABLE AS
+RETURN	
+	SELECT P.ID, P.Nombre, P.Apellidos, COUNT(V.ID) AS Subidas FROM LM_Viajes AS V
+	INNER JOIN LM_Tarjetas AS T
+	ON V.IDTarjeta = T.ID
+	INNER JOIN LM_Pasajeros AS P
+	ON P.ID = T.IDPasajero
+	
+	WHERE MomentoEntrada BETWEEN @Principio AND @Fin
+	GROUP BY P.ID, P.Nombre, P.Apellidos
+GO
+
+SELECT * FROM subidasUsuarios('24-02-2017','28-02-2017') ORDER BY ID
+SELECT * FROM LM_Viajes
+GO
+
+/*
+-3-
+Crea una función inline a la que pasemos la matrícula de un tren y una fecha de inicio 
+y fin y nos devuelva una tabla con el número de veces que ese tren ha estado en cada estación, 
+además del ID, nombre y dirección de la estación
+*/
+
+CREATE OR ALTER FUNCTION estacionesRecorridasPorTren(@MatriculaTren char(7), @FInicio smalldatetime, @FFin smalldatetime)
+RETURNS TABLE AS
+RETURN
+	SELECT E.ID, E.Denominacion, E.Direccion, ISNULL(COUNT(R.Momento),0) AS EstacionesRecorridas  FROM LM_Trenes AS T
+	INNER JOIN LM_Recorridos AS R
+	ON R.Tren = T.ID
+	RIGHT JOIN LM_Estaciones AS E
+	ON E.ID = R.estacion
+	WHERE T.Matricula = @MatriculaTren
+		AND R.Momento BETWEEN @FInicio AND @FFin
+	GROUP BY E.ID, E.Denominacion, E.Direccion
+GO
+
+SELECT * FROM LM_Trenes
+SELECT * FROM estacionesRecorridasPorTren('5607GLZ','24-02-2017','28-02-2017')
+
+/*
+-4-
+Crea una función inline que nos diga el número de personas que han pasado por una estacion en un periodo de tiempo. 
+Se considera que alguien ha pasado por una estación si ha entrado o salido del metro por ella. El principio y el fin 
+de ese periodo se pasarán como parámetros
+*/
