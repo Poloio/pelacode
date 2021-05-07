e0a4dd9db31e41c10055ebc95dc827223669667f
Poloio <carlos.aragon@iesnervion.es>
2021-05-07 23:53:48 +0200

    Ej 1 y 2 boletin 11_2 BBDD

--- SQL/programacion_bbdd/boletin11_2.sql
+++ SQL/programacion_bbdd/boletin11_2.sql
@@ -47,3 +47,109 @@ BEGIN CATCH
 END CATCH
 
 SELECT * FROM LM_Tarjetas
+GO
+/*
+-0-
+Crea un procedimiento RecargarTarjeta que reciba como parámetros el ID de una tarjeta y un importe 
+y actualice el saldo de la tarjeta sumándole dicho importe, además de grabar la correspondiente recarga
+*/
+
+CREATE OR ALTER PROCEDURE RecargarTarjeta @IdTarjeta int, @Importe smallmoney
+AS
+BEGIN
+	BEGIN TRANSACTION
+	BEGIN TRY
+		UPDATE LM_Tarjetas
+		SET Saldo += @Importe
+		WHERE ID = @IdTarjeta
+
+		DECLARE @NuevoSaldo smallmoney
+		SELECT @NuevoSaldo = Saldo
+		FROM LM_Tarjetas
+		WHERE ID = @IdTarjeta
+
+		INSERT INTO LM_Recargas
+		VALUES (NEWID(), @IdTarjeta, @Importe, CURRENT_TIMESTAMP, @NuevoSaldo)
+
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+		ROLLBACK
+	END CATCH 
+END
+GO
+
+EXEC RecargarTarjeta 1, 99
+GO
+
+/*
+-2-
+Crea un procedimiento almacenado llamado PasajeroSale que reciba como parámetros el ID de una tarjeta, 
+el ID de una estación y una fecha/hora (opcional). 
+
+El procedimiento se llamará cuando un pasajero 
+pasa su tarjeta por uno de los tornos de salida del metro. 
+Su misión es grabar la salida en la tabla LM_Viajes. 
+Para ello deberá localizar la entrada que corresponda, que será la última entrada 
+correspondiente al mismo pasajero y hará un update de las columnas que correspondan. 
+
+Si no existe la entrada, grabaremos una nueva fila en LM_Viajes dejando a NULL la estación y el momento de entrada.
+Si se omite el parámetro de la fecha/hora, se tomará la actual.
+También debe actualizarse el saldo de la tarjeta, descontando el importe del viaje grabado.
+*/
+CREATE OR ALTER PROCEDURE AplicarCoste @IDTarjeta int, @Coste smallmoney
+AS
+BEGIN
+	UPDATE LM_Tarjetas
+		SET Saldo -= @Coste
+		WHERE ID = @IdTarjeta
+END
+GO
+
+CREATE OR ALTER PROCEDURE PasajeroSale 
+	@IDTarjeta int, @IDEstacionSalida smallint
+AS
+BEGIN
+BEGIN TRANSACTION
+	
+BEGIN TRY
+	
+	IF EXISTS	(
+				SELECT IDEstacionEntrada FROM LM_Viajes 
+				WHERE IDTarjeta = @IDTarjeta
+				AND DATEDIFF(HH,MomentoEntrada,CURRENT_TIMESTAMP) <= 24
+				)
+	BEGIN
+		UPDATE LM_Viajes
+		SET IDEstacionSalida = @IDEstacionSalida,
+			MomentoSalida = CURRENT_TIMESTAMP
+		WHERE IDTarjeta = @IDTarjeta AND MomentoEntrada = (SELECT MAX(MomentoEntrada) FROM LM_Viajes)
+	END--IF
+	ELSE
+	BEGIN
+		INSERT INTO LM_Viajes (IDTarjeta, IDEstacionEntrada, IDEstacionSalida, MomentoEntrada, MomentoSalida, Importe_Viaje)
+		VALUES (
+		@IDTarjeta
+		,NULL
+		,@IDEstacionSalida
+		,NULL
+		,CURRENT_TIMESTAMP
+		,(SELECT Precio_Zona FROM LM_Zona_Precios WHERE Zona = (SELECT Zona_Estacion FROM LM_Estaciones 
+																WHERE ID = @IDEstacionSalida))
+		)
+	END--ELSE
+
+	DECLARE @Coste smallmoney
+		SELECT @Coste =  Importe_Viaje FROM LM_Viajes 
+		WHERE MomentoEntrada = (SELECT MAX(MomentoEntrada) FROM LM_Viajes)
+		AND IDTarjeta = @IDTarjeta
+
+	EXEC AplicarCoste @IDTarjeta, @Coste
+END TRY
+BEGIN CATCH
+	ROLLBACK
+END CATCH
+END
+GO
+
+EXEC PasajeroSale 1, 1
